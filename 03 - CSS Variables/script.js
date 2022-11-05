3e9f6f55abe870e772cd4eff7a719c878cc4a436
Sumeyya Javaid <jasum@protonmail.com>
2022-11-05 15:01:51 +0000

    Update script file
    Created and updated script.js file to change css of html elements

--- 03 - CSS Variables/script.js
+++ 03 - CSS Variables/script.js
@@ -1,8 +1,8 @@
-const inputs = document.querySelectorAll('.controls input');
+const inputs = document.querySelectorAll('.controls input'); // gives us a NodeList
 
 function handleUpdate() {
     // console.log(this.value);
-    const suffix = this.dataset.sizing || '';
+    const suffix = this.dataset.sizing || ''; // get data attribute sizing from the dataset by creating a suffix variable
     //console.log(suffix);
     document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
 }
