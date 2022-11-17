b3f0cacd280619df6af219932b04288bc02daac9
Sumeyya Javaid <jasum@protonmail.com>
2022-11-17 00:11:45 +0000

    Create script.js

---
+++ 09 - Dev Tools Domination/script.js
@@ -0,0 +1,33 @@
+const children = [{ name: 'susan', age: 2 }, { name: 'hugo', age: 8 }];
+
+function makeGreen() {
+    const p = document.querySelector('p');
+    p.style.color = '#BADA55';
+    p.style.fontSize = '50px';
+}
+
+// Regular
+console.log('hi');
+
+// Interpolated
+console.log(`Hi I am ${var}`);
+
+// Styled
+console.log('%c I am some cool text, )
+        // warning!
+
+        // Error :|
+
+        // Info
+
+        // Testing
+
+        // clearing
+
+        // Viewing DOM Elements
+
+        // Grouping together
+
+        // counting
+
+        // timing
