67ff6429adf1dfe060df33901692de238f780fee
Sumeyya Javaid <jasum@protonmail.com>
2022-11-18 22:08:02 +0000

    Update script.js

--- 10 - Hold Shift and Check Checkboxes/script.js
+++ 10 - Hold Shift and Check Checkboxes/script.js
@@ -1,30 +1,28 @@
 // select all checkboxes so we can listen to each of them
-const checkboxes = document.querySelectorAll('.inbox input[type=checkbox');
+const checkboxes = document.querySelectorAll('.inbox input[type=checkbox]');
 
-// check first one and put into a variable because when we check last one and hold down shift we need to know the last checked box
 let lastChecked;
 
 function handleCheck(e) {
-    // console.log(e);
+    //console.log(e);
+    // lastChecked = this;
+
     let inBetween = false;
 
     // check if they had the shift key down
-    // AND check to see if they are checking that it is down
-
+    // AND check to see if they are checking the box
     if (e.shiftKey && this.checked) {
-        // add what you please
-        // loop over every single checkbox
         checkboxes.forEach(checkbox => {
             console.log(checkbox);
             if (checkbox === this || checkbox === lastChecked) {
                 inBetween = !inBetween;
-                console.log('starting to check them in between');
+                console.log('check in between items on the list');
             }
+
             if (inBetween) {
                 checkbox.checked = true;
             }
-        });
-
+        })
     }
     lastChecked = this;
 }
