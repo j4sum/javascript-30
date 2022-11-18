ca6ff9d93412e9c9e447e394082bd9dfd1d08c1c
Sumeyya Javaid <jasum@protonmail.com>
2022-11-18 00:35:29 +0000

    Update script.js

--- 09 - Dev Tools Domination/script.js
+++ 09 - Dev Tools Domination/script.js
@@ -1,4 +1,4 @@
-const children = [{ name: 'susan', age: 2 }, { name: 'hugo', age: 8 }];
+const cats = [{ name: 'pickle', age: 2 }, { name: 'boba', age: 8 }];
 
 function makeGreen() {
     const p = document.querySelector('p');
@@ -10,24 +10,54 @@ function makeGreen() {
 console.log('hi');
 
 // Interpolated
-console.log(`Hi I am ${var}`);
+console.log('Hello, I am a %s string!', '🤡')
+    // console.log(`I am a string ${var}`) // ES6
 
 // Styled
-console.log('%c I am some cool text, )
-        // warning!
+// console.log('%c I am a great string', 'font-size:50px; background:red; text-shadow: 10px 10px 0 blue')
 
-        // Error :|
+// warning!
+console.warn('Oh Noooo!'); // stack trace where it got called
 
-        // Info
+// Error :|
+console.error('Oh shoot'); // stack trace
 
-        // Testing
+// Info
+console.info('UK is an Island');
 
-        // clearing
+// Testing
+const p = document.querySelector('p');
+console.assert(p.classList.contains('ouch'), 'That is wrong');
 
-        // Viewing DOM Elements
+// clearing
+console.clear(); // clear console
 
-        // Grouping together
+// Viewing DOM Elements
+console.log(p); // shows actual element
+console.dir(p); // opens DOM to look at properties and methods
 
-        // counting
+// Grouping together
+cats.forEach(cat => {
+    console.groupCollapsed(`${cats}`);
+    console.log(`${cat.name} is ${cat.age} years old`);
+    console.log(`${cat}.name} is ${cat.age * 7} cat years old`);
+    console.groupEnd(`${cat.name}`);
+});
 
-        // timing
+// counting
+console.count('wes'); // counts however many times you use a specific word, num, object, DOM node
+console.count('wes');
+console.count('alex');
+console.count('wes');
+
+// timing
+console.time('fetching data'); // start timer to fetch data
+fetch('https://api.github.com/users/wesbos') // fetch data from github
+    .then(data => data.json())
+    .then(data => {
+        console.timeEnd('fetching data'); // pass same string that was used to start it
+        console.log(data); //log data at that time
+    });
+
+//table
+console.table() //take an array of objects (cats), object (each cat) and assuming they have same properties: name, age display table
